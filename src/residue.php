ce2541d2bda69fdfb0f635b68d8048c943e60585
Majid <mkhan3189@gmail.com>
2018-01-30 23:16:34 +1100

    base

---
+++ src/residue.php
@@ -0,0 +1,12 @@
+//
+// Official PHP client library for Residue logging server
+//
+// Copyright 2017-present Muflihun Labs
+//
+// Author: @abumusamq
+//
+// https://muflihun.com
+// https://muflihun.github.io/residue
+// https://github.com/muflihun/residue-php
+//
+
